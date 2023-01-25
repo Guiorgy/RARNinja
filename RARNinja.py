1881a27cc98ace4098ec207283e4fea11b7d65dc
Guiorgy <Guiorgy123@gmail.com>
2023-01-26 03:40:51 +0400

    use dictionary generators to save memory

--- RARNinja.py
+++ RARNinja.py
@@ -1,8 +1,9 @@
 import math
 import os
 import time
-from itertools import product
+from itertools import islice
 from multiprocessing import Pool, Manager, cpu_count
+from typing import Optional, Sequence, Iterable
 
 import colorama
 import rarfile
@@ -46,16 +47,187 @@ def count_lines(file_path, encoding='utf-8'):
 
 
 def chunkify(source, *, chunk_size=None, chunks=None):
+    source_length = len(source)
+
     if chunk_size is None:
         if chunks is None:
             raise ValueError
 
-        chunk_size = math.floor(len(source) / chunks)
-        if chunk_size * chunks < len(source):
+        chunk_size = math.floor(source_length / chunks)
+        if chunk_size * chunks < source_length:
             chunk_size += 1
 
-    for i in range(0, len(source), chunk_size):
+    for i in range(0, source_length, chunk_size):
         yield source[i:(i + chunk_size - 1)]
+
+
+class DictionaryGenerator:
+    def _new_state(self, start=0, stop=None):
+        return islice(self._alphabet, start, stop)
+
+    def __init__(self, alphabet: str, word_length: int, start: Optional[int] = None, stop: Optional[int] = None):
+        if not alphabet:
+            raise ValueError('alphabet can\'t be empty')
+        if word_length < 1:
+            raise ValueError('word_length has to be positive')
+
+        self._alphabet = alphabet
+        self._word_length = word_length
+
+        alphabet_size = len(alphabet)
+        dictionary_size = pow(alphabet_size, word_length)
+
+        if start is not None and not (0 <= start < dictionary_size):
+            raise ValueError(f'start needs to be between 0 and dictionary_size({dictionary_size})')
+        if stop is not None and not ((start if start else 0) <= stop):
+            raise ValueError(f'stop can\'t be less than start')
+
+        if stop is None or stop >= dictionary_size:
+            stop = dictionary_size - 1
+
+        states = [None] * word_length
+
+        if start is None or start == 0:
+            word_count = 0
+
+            states = [self._new_state() for _ in range(word_length)]
+
+        else:
+            _start = word_count = start
+
+            i = word_length - 1
+
+            while i >= 0 and start:
+                offset = _start % alphabet_size
+
+                states[i] = self._new_state(start=offset)
+
+                _start -= offset
+                _start = math.floor(_start / alphabet_size)
+
+                i -= 1
+
+            while i >= 0:
+                states[i] = self._new_state()
+                i -= 1
+
+        generated_word = ''
+        for i in range(self._word_length - 1):
+            generated_word += next(states[i])
+
+        self._states = states
+        self._word_count = word_count
+        self._stop = stop
+        self._length = stop - start + 1
+        self._generated_word = generated_word
+        self._last_index = self._word_length - 1
+        self._current_index = self._last_index
+
+    def __next__(self):
+        while True:
+            if self._current_index >= 0 and self._word_count <= self._stop:
+                try:
+                    word = self._generated_word + next(self._states[self._last_index])
+                    self._word_count += 1
+                    return word
+                except StopIteration:
+                    self._states[self._last_index] = self._new_state()
+
+                    word_changed_part = ''
+                    self._current_index = self._last_index - 1
+                    while self._current_index >= 0:
+                        try:
+                            word_changed_part = next(self._states[self._current_index]) + word_changed_part
+                            self._generated_word = self._generated_word[
+                                                   :-(self._last_index - self._current_index)] + word_changed_part
+                            break
+                        except StopIteration:
+                            self._states[self._current_index] = self._new_state()
+                            word_changed_part = next(self._states[self._current_index]) + word_changed_part
+                            self._current_index -= 1
+            else:
+                raise StopIteration
+
+    def __iter__(self):
+        return self
+
+    def __len__(self):
+        return self._length
+
+
+def dictionary_chunk_generators(alphabet: str, word_length: int, chunks: int, chunk: Optional[int] = None):
+    if not (chunk is None or 0 <= chunk < chunks):
+        raise ValueError
+
+    alphabet_size = len(alphabet)
+    dictionary_size = pow(alphabet_size, word_length)
+
+    chunk_size = math.floor(dictionary_size / chunks)
+    if chunk_size * chunks < dictionary_size:
+        chunk_size += 1
+
+    generators = [
+        DictionaryGenerator(alphabet, word_length, chunk_start, chunk_start + chunk_size - 1)
+        for i, chunk_start in enumerate(range(0, dictionary_size, chunk_size)) if chunk is None or i == chunk
+    ]
+
+    if chunk is None:
+        return generators
+    elif generators:
+        return generators[0]
+    else:
+        return None
+
+
+class ChainGenerator(Iterable):
+    def __init__(self, generators: Sequence[DictionaryGenerator]):
+        self._length = 0
+        for gen in generators:
+            self._length += len(gen)
+
+        self._generators = generators
+        self._index = 0
+        self._stop = len(generators)
+
+    def __next__(self):
+        while True:
+            try:
+                return next(self._generators[self._index])
+            except StopIteration:
+                self._index += 1
+
+                if self._index == self._stop:
+                    raise
+
+    def __iter__(self):
+        return self
+
+    def __len__(self):
+        return self._length
+
+
+def dictionary_chunk_generators_ranged_length(alphabet: str, min_word_length: int, max_word_length: int, chunks: int, chunk: Optional[int] = None):
+    if max_word_length < min_word_length:
+        raise ValueError
+    if not (chunk is None or 0 <= chunk < chunks):
+        raise ValueError
+
+    generators = [[] for _ in range(chunks)]
+
+    for word_length in range(min_word_length, max_word_length + 1):
+        gens = dictionary_chunk_generators(alphabet, word_length, chunks, chunk)
+
+        if chunk is None:
+            for i in range(len(gens)):
+                if gens[i]:
+                    generators[i].append(gens[i])
+        elif gens:
+            generators[chunk].append(gens)
+
+    if chunk is None:
+        return [ChainGenerator(gen) for gen in generators]
+    else:
+        return ChainGenerator(generators[chunk])
 
 
 def dictionary_crack(RAR, dictionary, stop_event, progress=False, thread=0):
@@ -269,10 +441,6 @@ if __name__ == '__main__':
                 print('\nThe character set can\'t be empty. Try again.\n')
                 alphabet = None
 
-        dictionary = []
-        for i in range(min_len, max_len + 1):
-            dictionary += [''.join(t) for t in product(alphabet, repeat=i)]
-
     showProgress = None
     while showProgress is None:
         print('\nShow progress?')
@@ -311,11 +479,16 @@ if __name__ == '__main__':
     tries = 0
     clear_screen()
     with Manager() as manager:
-        with Pool(threads) as pool:
-            # TODO: handle CTRL^C termination
-            stop_event = manager.Event()
-            args = [(RAR, chunk, stop_event, showProgress, i) for i, chunk in enumerate(chunkify(dictionary, chunks=threads))]
+        stop_event = manager.Event()
 
+        if dictionary:
+            args = [(RAR, chunk, stop_event, showProgress, thread) for thread, chunk in enumerate(chunkify(dictionary, chunks=threads)) if chunk]
+        else:
+            generators = dictionary_chunk_generators_ranged_length(alphabet, min_len, max_len, threads)
+            args = [(RAR, generators[thread], stop_event, showProgress, thread) for thread in range(threads)]
+
+        with Pool(len(args)) as pool:
+            # TODO: handle CTRL^C termination
             start = time.time()
             for password, _tries in pool.starmap(dictionary_crack, args):
                 if password:
